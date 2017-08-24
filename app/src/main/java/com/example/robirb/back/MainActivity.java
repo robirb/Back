20e18aff65950d47fe9c4ce1dd9513dbf133036a
robirb <rrobirb@gmail.com>
2017-08-24 18:41:10 +0600

    Change

--- app/src/main/java/com/example/robirb/back/MainActivity.java
+++ app/src/main/java/com/example/robirb/back/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.robirb.back;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
                 if(name.equals("")){
                     Toast.makeText(MainActivity.this, "Name Field is Empty", Toast.LENGTH_SHORT).show();
+                    Log.d("hhhh","hhh");
                     return;
                 }
 
--- app/src/main/java/com/example/robirb/back/ThirdActivity.java
+++ app/src/main/java/com/example/robirb/back/ThirdActivity.java
@@ -2,6 +2,7 @@ package com.example.robirb.back;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +15,8 @@ public class ThirdActivity extends AppCompatActivity {
         setContentView(R.layout.activity_third);
 
         button_back= (Button) findViewById(R.id.button_back);
+
+        Log.d("hhhh","hhh");
 
         button_back.setOnClickListener(new View.OnClickListener() {
             @Override
